fdb328761608d770e2eb31c934435daeecd444a6
Haura <hauraaudifa05@gmail.com>
2024-10-31 15:02:52 +0700

    update

---
+++ main.py
@@ -0,0 +1,59 @@
+from ParkinsonClassification import logger
+from ParkinsonClassification.pipeline.pipeline_data_ingestion import DataIngestionTrainingPipeline
+from ParkinsonClassification.pipeline.pipeline_prepare_base_model import PrepareBaseModelTrainingPipeline
+from ParkinsonClassification.pipeline.pipeline_model_training import ModelTrainingPipeline
+from ParkinsonClassification.pipeline.pipeline_model_evaluation import EvaluationPipeline
+
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/ParkinsonClassification/components/data_ingestion.py
@@ -0,0 +1,38 @@
+import os
+import zipfile
+from shutil import copyfile
+from ParkinsonClassification import logger
+from ParkinsonClassification.utils.common import get_size
+from ParkinsonClassification.entity.config_entity import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self) -> str:
+        '''
+        Menggunakan file ZIP dari path lokal
+        '''
+        try: 
+            source_zip_path = self.config.source_local_file  # Path lokal file ZIP sumber
+            zip_download_dir = self.config.local_data_file  # Path tujuan untuk menyimpan ZIP
+            os.makedirs("artifacts/data_ingestion", exist_ok=True)
+
+            # Salin file ZIP dari path lokal ke tujuan
+            copyfile(source_zip_path, zip_download_dir)
+
+            logger.info(f"Copied data from {source_zip_path} to {zip_download_dir}")
+
+        except Exception as e:
+            raise e
+
+    def extract_zip_file(self):
+        """
+        zip_file_path: str
+        Ekstrak file ZIP ke direktori data
+        Fungsi ini tidak mengembalikan nilai
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
